7a54ddda42b4f8320ae4426b0a66d17d9fec6b0d
alexlar163 <alexlar163@gmail.com>
2022-03-14 19:38:25 -0500

    update code for connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,10 +3,13 @@ import { MongoClient } from "mongodb";
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+let connection: any;
 
 const connectDB = async () => {
+  if (connection) {
+    return connection;
+  }
   try {
-    let connection: any;
     const client = await MongoClient.connect(url);
     connection = client.db(DB_NAME);
     return connection;
